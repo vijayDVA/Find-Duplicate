dda9b4ba5fd716f838627e3db1b9040e2fd42d19
vijayDVA <48407258+vijayDVA@users.noreply.github.com>
2021-04-07 21:31:50 +0530

    COMMIT1

---
+++ src/FindDupDao.java
@@ -0,0 +1,68 @@
+import java.sql.Connection;
+import java.util.List;
+import java.util.Map;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class FindDupDao {
+
+	private String dbUrl = "jdbc:mysql://localhost:3306/duplists";
+	private String dbUname = "root";
+	private String dbPassword = "1234";
+	private String dbDriver = "com.mysql.cj.jdbc.Driver";
+	
+	public void loadDriver(String dbDriver)
+	{
+		try {
+			Class.forName(dbDriver);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public Connection getConnection()
+	{
+		Connection con = null;
+		try {
+			con = DriverManager.getConnection(dbUrl, dbUname, dbPassword);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return con;
+	}
+	
+	public void insert(Map<String, List<String>> lists) throws SQLException
+	{
+		
+		loadDriver(dbDriver);
+		Connection con = getConnection();
+		String sql = "insert into dupdb(File_locations) values(?)";
+		
+		PreparedStatement ps;
+		
+		if(con != null) {			
+		
+		ps = con.prepareStatement(sql);
+		for (List<String> list : lists.values()) {
+            if (list.size() > 1) {
+            	
+            	String location =list.toString();
+            	ps.setString(1, location);
+            	ps.executeUpdate();
+                //System.out.println("\n");
+            	
+                //for (String file : list) {
+                //    System.out.println(file);
+                   
+                //}
+            }
+        }
+		
+		
+		con.close();
+			
+		}
+	}
+	
+}
---
+++ src/FindDupFiles.java
@@ -0,0 +1,177 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import org.apache.commons.io.FilenameUtils;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+
+public class FindDupFiles 
+{
+	public static Set<String> emptyFiles = new HashSet<String>();
+	public static Set<String> exceptns = new HashSet<String>();
+	public static Map<String,Set<String>> finalMap = new HashMap<String,Set<String>>();
+	
+	private static MessageDigest messageDigest;
+    static {
+        try {
+            messageDigest = MessageDigest.getInstance("MD5");
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException("cannot initialize SHA-512 hash function", e);
+        }
+    }
+    
+
+	public static void main(String[] args) throws Exception 
+	{
+		//FindDupDao Dao = new FindDupDao();
+		Map<Long, HashMap<String,List<String>>> lists = new HashMap<Long, HashMap<String,List<String>>>();
+	      
+			System.out.println("C started ...");
+			//parseAllFiles(lists,"C:\\");
+	
+			System.out.println("D started ...");
+			parseAllFiles(lists,"D:\\");
+			
+			System.out.println("E started ...");
+			parseAllFiles(lists,"E:\\");
+			
+			for (String name: finalMap.keySet()) {
+			    String value = finalMap.get(name).toString();
+			    System.out.println(name + " " + value);
+			}
+			System.out.println("Executed Successfully..");
+		      System.out.println("These files are used by others ..");
+		       for (String value : exceptns)
+		            System.out.println(value + ", ");
+	   }
+
+
+	private static void parseAllFiles(Map<Long, HashMap<String, List<String>>> lists, String parentDirectory) throws Exception 
+	{
+		File[] filesInDirectory = new File(parentDirectory).listFiles();
+        if(filesInDirectory != null) 
+        {
+        	for (File dirChild : filesInDirectory)
+	        { 
+        		if(dirChild.isDirectory())
+	        	{
+                parseAllFiles(lists,dirChild.getAbsolutePath());
+	        	}
+        		else 
+        		{
+        			try
+        			{
+        				System.out.println(dirChild.getAbsolutePath());
+	        			long size = dirChild.length();
+	        			if(size!=0)
+	        			{
+	        				String ext = FilenameUtils.getExtension(dirChild.getAbsolutePath());
+	        				if(ext=="")
+	        					ext="txxt";
+	        				HashMap<String,List<String>> extMap = lists.get(size);
+	        				List<String> paths;
+	        				Boolean sts = true;
+	        				if(extMap==null)
+	        				{	        					
+	        					extMap = new HashMap<String,List<String>>();
+	        					paths = new ArrayList<String>();
+	        					paths.add(dirChild.getAbsolutePath());
+	        					extMap.put(ext, paths);
+	        					lists.put(size, extMap);
+	        					sts = false;
+	        				}
+	        				      				
+	        				if(lists.get(size).get(ext) == null)
+	        				{
+	        					paths = new ArrayList<String>();
+	        					paths.add(dirChild.getAbsolutePath());
+	        					extMap.put(ext, paths);
+	        					lists.put(size, extMap);
+	        					sts = false;
+	        				}
+	        				if(sts) 
+	        				{
+	        					paths = lists.get(size).get(ext);
+	        					for(String locations :paths)
+	        					{ 
+	        						RandomAccessFile raf1 = new RandomAccessFile(locations, "r");
+	        						RandomAccessFile raf2 = new RandomAccessFile(dirChild.getAbsoluteFile(), "r");
+	        						File locationFile = new File(locations);
+	        						
+	        				        byte[] buffer1 = new byte[100];
+	        				        byte[] buffer2 = new byte[100];
+	        				        raf1.read(buffer1,0,100);
+	        				        raf2.read(buffer2,0,100);
+	        				        if(!Arrays.equals(buffer1,buffer2))
+	        				        	continue;
+	        				        raf1.read(buffer1,0,100);
+	        				        raf2.read(buffer2,0,100);
+	        				        if(!Arrays.equals(buffer1,buffer2))
+	        				        {
+	        				        	continue;
+	        				        }
+	        				        else
+	        				        {
+	        				        	String uniqueFile1 = makeHashQuick(locationFile);
+	        				        	String uniqueFile2 = makeHashQuick(dirChild);
+	        				        	if(uniqueFile1.equals(uniqueFile2))
+	        				        	{
+	        				        		Set<String> dup = finalMap.get(uniqueFile1);
+	        				        		if(dup == null)
+	        				        		{
+	        				        			dup = new HashSet<String>();
+	        				        		}
+	        				        		dup.add(locations);
+	        				        		dup.add(dirChild.getAbsolutePath());
+	        				        		finalMap.put(uniqueFile1, dup);
+	        				        	}
+	        				        }
+	        				       
+	        					}
+	        					paths.add(dirChild.getAbsolutePath());
+		        				extMap.put(ext, paths);
+		        				lists.put(size, extMap);
+	        				}
+	
+	        			}
+	        			else
+	        			{
+	        				emptyFiles.add(dirChild.getAbsolutePath());
+	        			}
+        			}
+        			catch(Exception e)
+					{			
+						exceptns.add(e.toString() +" = "+dirChild);
+						//throw e;
+        				
+					}
+        		}
+	        }
+        }
+        
+        
+	}
+
+
+	private static String makeHashQuick(File infile) throws IOException 
+	{
+		  FileInputStream fin = new FileInputStream(infile);
+	        byte data[] = new byte[(int) infile.length()];
+	        fin.read(data);
+	        fin.close();
+	        String hash = new BigInteger(1, messageDigest.digest(data)).toString(16);
+	        return hash;
+	
+	}
+
+}
